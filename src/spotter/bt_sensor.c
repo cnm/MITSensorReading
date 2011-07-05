0481da4bacc770ebf5aad78c3930e088afdc0a30
António Novais <antonio.novais@nwc.pt>
2011-07-05 22:46:10 +0100

    BT sensor plugin

--- src/spotter/bt_sensor.c
+++ src/spotter/bt_sensor.c
@@ -6,24 +6,74 @@
 #include <errno.h>
 #include <stddef.h>
 #include <pthread.h>
-#include <unistd.h>
-#include <netdb.h>
 #include <syslog.h>
 #include <stdint.h>
-#include <time.h>
+#include <sys/socket.h>
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/hci.h>
+#include <bluetooth/hci_lib.h>
 #include <fred/handler.h>
 #include "spotter.h"
 #include "location.h"
 
 void (* sensor_result)(SensorData *);
+static pthread_t sense_thread;
 
 void print_state(){
 
 }
 
-void start_sense(){
+void * sense(){
 	SensorData data;
-	data->type = RSS;
+	data.type = COUNT;
+
+	inquiry_info *ii = NULL;
+
+	int max_rsp, num_rsp;
+	int dev_id, sock, len, flags;
+	int i;
+	char addr[19] = { 0 };
+	char name[248] = { 0 };
+
+	dev_id = hci_get_route(NULL);
+	sock = hci_open_dev( dev_id );
+	if (dev_id < 0 || sock < 0) {
+		perror("opening socket");
+		exit(1);
+	}
+
+	len  = 8;
+	max_rsp = 255;
+	flags = IREQ_CACHE_FLUSH;
+	ii = (inquiry_info*)malloc(max_rsp * sizeof(inquiry_info));
+
+	num_rsp = hci_inquiry(dev_id, len, max_rsp, NULL, &ii, flags);
+	if( num_rsp < 0 )
+		perror("hci_inquiry");
+	else
+		data.people = num_rsp;
+
+	/*for (i = 0; i < num_rsp; i++) {
+		ba2str(&(ii+i)->bdaddr, addr);
+		memset(name, 0, sizeof(name));
+		if (hci_read_remote_name(sock, &(ii+i)->bdaddr, sizeof(name),
+			name, 0) < 0)
+		strcpy(name, "[unknown]");
+		printf("%s  %s\n", addr, name);
+	}*/
+
+	free( ii );
+	close( sock );
+
+	sensor_result(&data);
+
+	sleep(1);
+
+	return NULL;
+}
+
+void start_sense(){
+	pthread_create(&sense_loop,NULL,loop,NULL);
 }
 
 void start_cb(void (* sensor_result_cb)(SensorData *)){
