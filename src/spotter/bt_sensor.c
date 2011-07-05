6a2876981a34f9d6c215a3f206bba651f2a3b9f1
António Novais <antonio.novais@nwc.pt>
2011-07-05 22:53:16 +0100

    fix bt plugin v0.1

--- src/spotter/bt_sensor.c
+++ src/spotter/bt_sensor.c
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <errno.h>
+#include <unistd.h>
 #include <stddef.h>
 #include <pthread.h>
 #include <syslog.h>
@@ -31,9 +32,6 @@ void * sense(){
 
 	int max_rsp, num_rsp;
 	int dev_id, sock, len, flags;
-	int i;
-	char addr[19] = { 0 };
-	char name[248] = { 0 };
 
 	dev_id = hci_get_route(NULL);
 	sock = hci_open_dev( dev_id );
@@ -73,7 +71,7 @@ void * sense(){
 }
 
 void start_sense(){
-	pthread_create(&sense_loop,NULL,loop,NULL);
+	pthread_create(&sense_thread,NULL,sense,NULL);
 }
 
 void start_cb(void (* sensor_result_cb)(SensorData *)){
@@ -82,5 +80,4 @@ void start_cb(void (* sensor_result_cb)(SensorData *)){
 
 void stop_cb(){
 	sensor_result = NULL;
-	sensor_loop = false;
 }
