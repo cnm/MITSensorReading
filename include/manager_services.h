ba7802fb847389dd4deba5e7c785d055712ee2bd
Antonio Novais <anovais@tagus.inesc-id.pt>
2011-10-11 20:17:44 +0100

    commit post dropbox rollback

--- include/manager_services.h
+++ include/manager_services.h
@@ -4,7 +4,7 @@
  *  Created on: Jun 24, 2011
  *      Author: root
  */
-
+ 
 #ifndef MANAGER_SERVICES_H_
 #define MANAGER_SERVICES_H_
 
@@ -14,6 +14,5 @@ void ConfirmSpotter(uint16_t spotter_address, Location location, unsigned short 
 void SpontaneousSpotter(uint16_t spotter_address, Location location, unsigned short max_frequency);
 void DeliverSpotterData(uint16_t spotter_address, LocationPacket * packet, uint64_t time_sent);
 void ConfirmSpontaneousRegister(uint16_t aggregator_address, unsigned short final_frequence);
-
 
 #endif /* MANAGER_SERVICES_H_ */
